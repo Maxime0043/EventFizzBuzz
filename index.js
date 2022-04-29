bb8dabd3d127d7908d1d05e823ca88b609bbe5e2
Pinna Maxime <max.pinna@hotmail.fr>
2022-04-29 12:30:05 +0200

    OK: Vérification du type d'objet envoyé lors de l'écoute

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 emitter.on("FizzBuzz", function (number) {
+  if (typeof number != "number") {
+    console.log(number);
+    return;
+  }
+
   console.log(number);
 });
 
 emitter.emit("FizzBuzz", 5);
+emitter.emit("FizzBuzz", "coucou");
