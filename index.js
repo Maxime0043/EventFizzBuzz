d42e9e19b998f64d0d35fb812eceada14486a6f2
Pinna Maxime <max.pinna@hotmail.fr>
2022-04-29 12:38:35 +0200

    OK: Si la valeur envoyé est un multiple de 3 -> Listener affiche 'Fizz'

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ emitter.on("FizzBuzz", function (number) {
 
   if (number % 5 == 0 && number % 3 == 0) console.log("FizzBuzz");
   else if (number % 5 == 0) console.log("Buzz");
+  else if (number % 3 == 0) console.log("Fizz");
   else console.log(number);
 });
 
 emitter.emit("FizzBuzz", 15);
 emitter.emit("FizzBuzz", 10);
 emitter.emit("FizzBuzz", 3);
+emitter.emit("FizzBuzz", 34);
+emitter.emit("FizzBuzz", "Message");
