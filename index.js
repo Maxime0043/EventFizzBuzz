a3225e25859f6c8596808ee97fd96a474de354ef
Pinna Maxime <max.pinna@hotmail.fr>
2022-04-29 12:28:32 +0200

    OK: Implémentation du Listener 'FizzBuzz' qui lit le nombre envoyé

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
+
+emitter.on("FizzBuzz", function (number) {
+  console.log(number);
+});
+
+emitter.emit("FizzBuzz", 5);
